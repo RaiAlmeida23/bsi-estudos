952244869c04f370325bd93fa22d4107c1d1db75
Raissa Almeida <raissa.dev23@gmail.com>
2026-09-14 17:01:44 -0300

    Update exx03.py

--- 1-periodo/programacao01/lvp-introducao/exx03.py
+++ 1-periodo/programacao01/lvp-introducao/exx03.py
@@ -1,3 +1,6 @@
+# Desenvolva um algoritmo que realize a leitura de um valor inteiro fornecido via teclado. 
+# O programa deve calcular o seu antecessor (o número que vem imediatamente antes dele na sequência numérica) e exibir esse resultado na tela.
+
 def main():
     num = int(0)
     ant = int(0)
