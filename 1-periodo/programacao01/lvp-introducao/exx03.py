ca7d5cf7f1589dba4c5e640c629a36503b72ef42
Raissa Almeida <raissa.dev23@gmail.com>
2026-09-14 16:57:04 -0300

    Create exx03.py

---
+++ 1-periodo/programacao01/lvp-introducao/exx03.py
@@ -0,0 +1,14 @@
+def main():
+    num = int(0)
+    ant = int(0)
+    
+    num = int(input())
+    
+    ant = num - 1
+    
+    print(f'{ant}')
+    
+    return 0
+    
+if __name__ == "__main__":
+    main()
