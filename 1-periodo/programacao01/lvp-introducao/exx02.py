ea65d565147aa778932b5707bbde79ba5190d50c
Raissa Almeida <raissa.dev23@gmail.com>
2026-09-14 16:55:29 -0300

    Create exx02.py

---
+++ 1-periodo/programacao01/lvp-introducao/exx02.py
@@ -0,0 +1,16 @@
+def main():
+    num1 = int(0)
+    num2 = int(0)
+    produto = int(0)
+    
+    num1 = int(input())
+    num2 = int(input())
+    
+    produto = num1 * num2
+    
+    print(f'{produto}')
+    
+    return 0
+    
+if __name__ == "__main__":
+    main()
