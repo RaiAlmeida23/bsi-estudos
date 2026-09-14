0ad8b239c5007182457d525abb1290258e96103e
Raissa Almeida <raissa.dev23@gmail.com>
2026-09-14 16:53:59 -0300

    Update exx01.py

--- 1-periodo/programacao01/lvp-introducao/exx01.py
+++ 1-periodo/programacao01/lvp-introducao/exx01.py
@@ -1,1 +1,15 @@
+def main():
+    num1 = int(0)
+    num2 = int(0)
+    soma = int(0)
+    
+    num1 = int(input())
+    num2 = int(input())
+    
+    soma = num1 + num2
+    
+    print(f'{soma}')
+    return 0
 
+if __name__ == "__main__":
+    main()
