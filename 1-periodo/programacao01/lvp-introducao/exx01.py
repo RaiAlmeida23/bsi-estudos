3b603293cc8d73588daa6ee4b50529fd1ef45f57
Raissa Almeida <raissa.dev23@gmail.com>
2026-09-14 17:00:12 -0300

    Update exx01.py

--- 1-periodo/programacao01/lvp-introducao/exx01.py
+++ 1-periodo/programacao01/lvp-introducao/exx01.py
@@ -1,3 +1,6 @@
+# Desenvolva um programa que seja capaz de realizar a leitura de dois números inteiros fornecidos pelo usuário
+# Após a leitura, o sistema deve calcular a soma entre esses valores e exibir o resultado final
+
 def main():
     num1 = int(0)
     num2 = int(0)
