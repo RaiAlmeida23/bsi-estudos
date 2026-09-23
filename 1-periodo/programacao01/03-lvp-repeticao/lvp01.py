f27aab63211f1faac655cf9e95165d1763e45f0e
Raissa Almeida <raissa.dev23@gmail.com>
2026-09-23 09:17:35 -0300

    Adiciona lvp de repetições

---
+++ 1-periodo/programacao01/03-lvp-repeticao/lvp01.py
@@ -0,0 +1,7 @@
+"""
+
+Desenvolva um algoritmo que seja capaz de imprimir na tela uma sequência numérica progressiva, iniciando no valor 0 e finalizando no valor 9. 
+Cada número deve ser apresentado em uma nova linha.
+
+"""
+
